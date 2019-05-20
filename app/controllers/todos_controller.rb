01837713f38335977f9f5ea9cd3267b3728600c6
Nicki Nixon <nickitza@gmail.com>
2019-05-20 16:36:50 -0600

    views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,48 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only:[:show, :edit, :update, :destroy]
+
   def index
+    @todos = current_user.todos
   end
 
   def show
   end
 
   def new
+    @todo = Todo.new()
+  end
+
+  def create
+    @todo = current_user.todos.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todo_path(@todo)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+  def set_todo
+    @todo = current_user.todos.find(params[:id])
+  end
+  def todo_params
+    params.require(:todo).permit(:title, :details)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :todos
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end

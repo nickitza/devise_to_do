8eab7fa69557457f07bb324e7eb0d990c8622e8f
Nicki Nixon <nickitza@gmail.com>
2019-05-20 14:44:49 -0600

    devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "todos#index"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :todos
 end
